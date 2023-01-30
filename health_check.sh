2cbd837676eb8d42332c2dc685f848dc124ce464
Jussi Kajalin <jkajalin@gmail.com>
2023-01-30 16:59:27 +0200

    uploading health_check file for testing

--- health_check.sh
+++ health_check.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-echo "Hello from shell script"
+##echo "Hello from shell script"
+VAR_A=$(curl https://jk-full-stack-pokedex.fly.dev/health | grep -o "ok")
+VAR_B="ok"
+
+echo
+if [ "$VAR_A" != "$VAR_B" ]; then
+    echo "Not equal ..."
+else [ "$VAR_A" = "$VAR_B" ]
+    echo "It's a match
 
 exit 0
