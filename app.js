a62af34ff0018485c6a9e500d269345ded5ad67a
Jussi Kajalin <jkajalin@gmail.com>
2023-02-03 16:44:39 +0200

    another commit #skip

--- app.js
+++ app.js
@@ -16,5 +16,5 @@ app.get('/health', (req, res) => {
 })
 
 app.get('/version', (req, res) => {
-  res.send('25') // change this string to ensure a new version deployed
+  res.send('26') // change this string to ensure a new version deployed
 })
