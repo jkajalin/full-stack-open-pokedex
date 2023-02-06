ffc04e8b71975178daa737b9011c9937c03a9215
Jussi Kajalin <jkajalin@gmail.com>
2023-02-06 12:06:09 +0200

    curl health check script. Broken build test

--- health_check.sh
+++ health_check.sh
@@ -2,7 +2,7 @@
 
 ##echo "Hello from shell script"
 #alias curl='fly curl'
-VAR_A=$(curl -v https://jk-full-stack-pokedex.fly.dev/health | grep -o "ok")
+VAR_A=$(curl -v https://jk-full-stack-pokedex.fly.dev/health | grep "ok")
 VAR_B="ok"
 
 echo
